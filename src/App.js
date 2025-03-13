aae3012f33a2963f2bb8b7f56b9d699b333aa09f
Tommy <tommyngo156@gmail.com>
2025-03-12 20:10:22 -0400

    booking functionality working and tested

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React from "react"
 import Nav from "./components/Nav"
+import Main from "./components/Main"
 import './App.css';
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Nav />
-    </>
+    <BrowserRouter>
+      <>
+        <Nav />
+        <Main />
+      </>
+    </BrowserRouter>
   );
 }
 
